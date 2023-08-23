8ecf6a9ce7335895733ae4d743274ab152c6fca2
Hoangzxczxc <139453473+Hoangzxczxc@users.noreply.github.com>
2023-08-24 02:59:55 +0700

    Delete create.blade.php

--- ManageMovie/resources/views/categories/create.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="/category" method="post">
-    @csrf
-    <label for="namecategory">Name Category</label>
-    <input type="text" name="namecategory" id="namecategory">
-    <br>
-    <input type="submit" value="Create">
-</form>
